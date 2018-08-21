ceb14d466c8ad687929404cff981cfb7f9c6c9f8
Vaibhav Joshi <vaibhavjoshi@Vaibhavs-iMac-907.local>
2018-08-21 12:18:53 -0400

    added new functions using while loop

--- max-min.js
+++ max-min.js
@@ -32,3 +32,31 @@ function highNumber(arr) {
 console.log(lowNumber(myArray));
 
 console.log(highNumber(myArray));
+
+// Max -Min Using while loop
+
+function MaxNumber(arr) {
+    let i=0;
+    let MaxNumber = arr[0];
+    while (i<arr.length){
+        if (arr[i]> MaxNumber){
+            MaxNumber = arr[i];
+
+        }
+        i++
+    }
+    return MaxNumber;
+}
+
+function MinNumber(arr) {
+    let i = 0;
+    let MinNumber = arr[0];
+    while (i < arr.length) {
+        if (arr[i] < MinNumber) {
+            MinNumber = arr[i];
+
+        }
+        i++
+    }
+    return MinNumber;
+}
